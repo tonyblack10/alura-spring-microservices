727bf80c77d5e8f7b84b2b0292bbb3613923dfe0
Tony Costa <tonyblack10@gmail.com>
2020-07-04 03:33:14 -0300

    implementação com Spring Feign

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,34 +1,23 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.stereotype.Service;
+
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
-
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
 	
-	private final RestTemplate client;
-	private final DiscoveryClient discoveryClient;
+	private final FornecedorClient fornecedorClient;
 
-	public CompraService(RestTemplate client, DiscoveryClient discoveryClient) {
-		this.client = client;
-		this.discoveryClient = discoveryClient;
+	public CompraService(FornecedorClient fornecedorClient) {
+		this.fornecedorClient = fornecedorClient;
 	}
 
     public void realizaCompra(CompraDTO compra) {
-        ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
-                HttpMethod.GET,
-                null,
-                InfoFornecedorDTO.class);
-        
-        discoveryClient.getInstances("fornecedor").stream()
-        	.forEach(fornecedor -> System.out.println("localhost:"+fornecedor.getPort()));
+    	InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 
-        System.out.println(exchange.getBody().getEndereco());
+        System.out.println(info.getEndereco());
     }
 }
