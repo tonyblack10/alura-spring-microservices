8d570df6e7b961c2e1d9aafb2f9c4a0018291f19
Tony Costa <tonyblack10@gmail.com>
2020-07-05 04:03:11 -0300

    integração entre serviços

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import java.time.LocalDate;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -7,9 +9,12 @@ import org.springframework.stereotype.Service;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.alura.microservice.loja.client.FornecedorClient;
+import br.com.alura.microservice.loja.client.TransportadorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
+import br.com.alura.microservice.loja.dto.InfoEntregaDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.dto.VoucherDTO;
 import br.com.alura.microservice.loja.model.Compra;
 import br.com.alura.microservice.loja.repository.CompraRepository;
 
@@ -22,8 +27,11 @@ public class CompraService {
 
 	private final CompraRepository compraRepository;
 
-	public CompraService(FornecedorClient fornecedorClient, CompraRepository compraRepository) {
+	private final TransportadorClient transportadorClient;
+
+	public CompraService(FornecedorClient fornecedorClient, TransportadorClient transportadorClient, CompraRepository compraRepository) {
 		this.fornecedorClient = fornecedorClient;
+		this.transportadorClient = transportadorClient;
 		this.compraRepository = compraRepository;
 	}
 
@@ -37,12 +45,21 @@ public class CompraService {
     	logger.info("realizando um pedido");
     	InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
 
-        System.out.println(info.getEndereco());
+    	InfoEntregaDTO entregaDTO = new InfoEntregaDTO();
+    	entregaDTO.setPedidoId(pedido.getId());
+    	entregaDTO.setDataParaEntrega(LocalDate.now().plusDays(pedido.getTempoDePreparo()));
+    	entregaDTO.setEnderecoOrigem(info.getEndereco());
+    	entregaDTO.setEnderecoDestino(compra.getEndereco().toString());
+    	
+        VoucherDTO voucher = transportadorClient.reservaEntrega(entregaDTO);
         
         Compra compraSalva = new Compra();
         compraSalva.setPedidoId(pedido.getId());
         compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
         compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+        compraSalva.setDataParaEntrega(voucher.getPrevisaoParaEntrega());
+        compraSalva.setVoucher(voucher.getNumero());
+        
         compraRepository.save(compraSalva);
         
         return compraSalva;
