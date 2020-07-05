5527bc34b8fc883276522361e57d7ed867b66fc0
Tony Costa <tonyblack10@gmail.com>
2020-07-05 05:45:32 -0300

    servidor de autenticação

---
+++ auth/src/main/java/br/com/alura/microservice/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package br.com.alura.microservice.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
